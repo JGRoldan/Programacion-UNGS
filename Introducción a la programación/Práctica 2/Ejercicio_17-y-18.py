7e50f08c936c511377e18e49eb9bd704268805b8
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-03-25 13:06:40 -0300

    Update Ejercicio_17-y-18.py

--- Introducción a la programación/Práctica 2/Ejercicio_17-y-18.py
+++ Introducción a la programación/Práctica 2/Ejercicio_17-y-18.py
@@ -35,13 +35,15 @@ b=int(input('Ingrese el coeficiente B: '))
 c=int(input('Ingrese el coeficiente C: '))
 print('La ecuacion es: ',a,'x^2','+',b,'x +',c,'\n')
 
-coefRaiz = pow(b,2) - (4*a*c)
-x1= (-b + sqrt(coefRaiz))/(2*a)
-x2= (-b - sqrt(coefRaiz))/(2*a)
-
-if(coefRaiz<0):
-    print('∄ solucion en los reales')
-elif(coefRaiz==0):
-    print('La unica raiz posible es', x1)
+if(a>0):
+    coefRaiz = pow(b,2) - (4*a*c)
+    if(coefRaiz<0):
+        print('∄ solucion en los reales')
+    elif(coefRaiz==0):
+        print('La unica raiz posible es', x1)
+    else:
+        x1= (-b + sqrt(coefRaiz))/(2*a)
+        x2= (-b - sqrt(coefRaiz))/(2*a)
+        print('La primer raiz es: ',x1,'\nLa segunda raiz es: ',x2)
 else:
-    print('La primer raiz es: ',x1,'\nLa segunda raiz es: ',x2)
+    print('a>0')
