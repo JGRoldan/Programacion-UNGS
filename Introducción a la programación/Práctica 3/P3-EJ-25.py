c5bb66ef57d71ee7f5ef89c1e401932840eca67b
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-04-16 10:47:30 -0300

    Update P3-EJ-25.py

--- Introducción a la programación/Práctica 3/P3-EJ-25.py
+++ Introducción a la programación/Práctica 3/P3-EJ-25.py
@@ -4,8 +4,19 @@
 
 palabra='JAVASCRIPT'
 filas=3
-for i in range(1,n+1):
+for i in range(1,filas+1):
   if i==1 or i==3:
     print('*'*((len(palabra))+4))
   else:
     print('*',palabra,'*')
+    
+ #-------------------------
+
+palabra='* '+'JAVASCRIPT'+' *'
+filas=3
+
+for i in range(1,filas+1):
+  if i==1 or i==3:
+    print('*'*(len(palabra)))
+  else:
+    print(palabra)
