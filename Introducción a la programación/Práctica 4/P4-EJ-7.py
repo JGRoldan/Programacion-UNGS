e9ca50ca22bd2e224e5193f4203c544f5a68b256
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-04-29 15:26:21 -0300

    Add files via upload

---
+++ Introducción a la programación/Práctica 4/P4-EJ-7.py
@@ -0,0 +1,28 @@
+# -------------------
+# ----------------- 7
+# -------------------
+
+# ----A
+
+def elevarAlCubo(x):
+  return x**3
+
+# ----B
+
+#alcubo.py
+
+# ----C
+#from alcubo.py import *
+
+print(0,'al cubo:',elevarAlCubo(0))
+print(1,'al cubo:',elevarAlCubo(1))
+print(2,'al cubo:',elevarAlCubo(2))
+print(3,'al cubo:',elevarAlCubo(3))
+print(4,'al cubo:',elevarAlCubo(4))
+print(5,'al cubo:',elevarAlCubo(5))
+print(6,'al cubo:',elevarAlCubo(6))
+print(-1,'al cubo:',elevarAlCubo(-1))
+print(-2,'al cubo:',elevarAlCubo(-2))
+print(-3,'al cubo:',elevarAlCubo(-3))
+print(-4,'al cubo:',elevarAlCubo(-4))
+print(-5,'al cubo:',elevarAlCubo(-5))
