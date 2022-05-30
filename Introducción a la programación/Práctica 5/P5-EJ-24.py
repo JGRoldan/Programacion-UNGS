7165ecb2825de222064289231f5c0fd315217eda
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-05-30 16:17:53 -0300

    Add files via upload

---
+++ Introducción a la programación/Práctica 5/P5-EJ-24.py
@@ -0,0 +1,31 @@
+def pagara(numeroCliente,localidad)
+	
+	# Funciones ya creadas
+
+	cobertura=cobertura(numeroCliente) #string oro o plata
+	cantidadDeUsos=usados(numeroCliente)#number cantidad de veces q uso el sistema
+	estaCubierto=radioDeCobertura(numeroCliente,localidad)#Boolean
+
+	if estaCubierto:
+		if cobertura=='oro':
+			print('El servicio es gratis') 
+		else:
+			if cantidadDeUsos<=5:
+				print('El servicio es gratis')
+			else:
+				print('El servicio serian $50')
+	else:
+		if cobertura=='oro':
+			print('El servicio serian $30')
+		else:
+			if cantidadDeUsos<=5:
+				print('El servicio serian $30')
+			else:
+				print('El servicio serian $50 + $30')
+
+# USUARIO
+
+numeroCliente=int(input('Ingresa el numero de cliente: '))
+localidad=input('Ingrese localidad: ')
+
+pagara(numeroCliente,localidad)
