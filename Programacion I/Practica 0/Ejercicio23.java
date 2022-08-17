fa89ce2cbabe8060827775c2bea33f145f6b51af
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-08-17 15:25:03 -0300

    Update Ejercicio23.java

--- Programacion I/Practica 0/Ejercicio23.java
+++ Programacion I/Practica 0/Ejercicio23.java
@@ -3,14 +3,26 @@ package practica_0;
 public class Ejercicio23 {
 
 	
-	public static boolean puedenColocarse(String a,String b,String c) {
-		if(a.charAt(1)==b.charAt(1) && a.charAt(3)==c.charAt(3)) return true;
+		public static boolean puedenColocarse(String a,String b,String c) {
+		
+		for(int i=0; i<b.length();i++) {
+			if(a.indexOf(b.charAt(i))!= -1) { //ubicamos a en b
+				i++; //salto un renglon
+				
+				for (int j=i+1;j<b.length();j++) {
+					if(c.indexOf(b.charAt(j))!= -1) {//ubicamos c en b
+						return true;
+					}
+				}
+				return false;
+			}
+		}
 		return false;
 	}
 	public static void main(String[] args) {
 		
-		String a="bueno";
-		String b="jugo";
+		String a="jugo";
+		String b="bueno";
 		String c="anana";
 		System.out.println(puedenColocarse(a,b,c));
 		
