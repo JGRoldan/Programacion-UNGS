2651d7bdf65fbeb9d5363d832d9e6059d0d55358
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-08-17 14:53:29 -0300

    Update Ejercicio19.java

--- Programacion I/Practica 0/Ejercicio19.java
+++ Programacion I/Practica 0/Ejercicio19.java
@@ -19,23 +19,8 @@ public class Ejercicio19 {
 		return false;
 	}
 	
-		//--------OTRA SOLUCION--------
-	public static boolean abecedaria(String s) {
-		String abecedario = "abcdefghijklmnopqrstuvwxyz";
-		
-		for(int i=0;i<s.length();i++) {
-			if (pos(abecedario,s.charAt(i)) < i) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	
 	public static void main(String[] args) {
 		System.out.println(esAbecedaria("agil"));
-		
-		System.out.println(abecedaria("agil"));
 		
 	}
 
