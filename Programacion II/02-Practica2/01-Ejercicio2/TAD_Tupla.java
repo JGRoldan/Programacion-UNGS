60cc49fb1bc8d8a891586d466f8ad0ea2333c914
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2023-04-10 15:02:54 -0300

    Update TAD_Tupla.java

--- Programacion II/02-Practica2/01-Ejercicio2/TAD_Tupla.java
+++ Programacion II/02-Practica2/01-Ejercicio2/TAD_Tupla.java
@@ -2,35 +2,37 @@ package tad_ej2;
 
 import java.util.ArrayList;
 
-public class TAD_Tupla{
+public class TAD_Tupla<K,V>{
 	
-	private int x; 
-	private int y;
+	private K elem1;
+	private V elem2;
 	
-	public TAD_Tupla(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public TAD_Tupla(K key, V value) {
+		this.elem1 = key;
+		this.elem2 = value;
+	}
+	
+	public K getElem1() {
+		return elem1;
 	}
 
-	public int getX() {
-		return x;
+	public void setElem1(K elem1) {
+		this.elem1 = elem1;
 	}
 
-	public int getY() {
-		return y;
-	}
-	public void setX(int X) {
-		this.x = X;
+	public V getElem2() {
+		return elem2;
 	}
 
-	public void setY(int Y) {
-		this.y = Y;
+	public void setElem2(V elem2) {
+		this.elem2 = elem2;
 	}
+
 
 	@Override
 	public String toString() {
-		return "TAD_Tupla [x=" + x + ", y=" + y + "]";
-	} 
+		return "Tupla [key=" + elem1 + ", value=" + elem2 + "]";
+	}
 	
 
 }
